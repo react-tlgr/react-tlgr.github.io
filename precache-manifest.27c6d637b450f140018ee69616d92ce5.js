1ae79cae996548ba69e456cac6381fff476a3ee6
Clin01 <clinshaiju@protonmail.com>
2021-05-04 22:35:52 -0500

    fix

---
+++ precache-manifest.27c6d637b450f140018ee69616d92ce5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ca2f481b7d7eef392bc31ac2afe89c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "623732b5ad8aa8a47f69",
+    "url": "/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "4160dfb46474fa92543b",
+    "url": "/static/css/4.a03e84ba.chunk.css"
+  },
+  {
+    "revision": "db8a30e653c541482a86",
+    "url": "/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "b1c48719f63f74aca66d",
+    "url": "/static/css/7.751a6c49.chunk.css"
+  },
+  {
+    "revision": "0c786683b7c6294ce15d",
+    "url": "/static/css/main.162c7196.chunk.css"
+  },
+  {
+    "revision": "623732b5ad8aa8a47f69",
+    "url": "/static/js/0.532e2705.chunk.js"
+  },
+  {
+    "revision": "95edc7f8504280833ce7",
+    "url": "/static/js/3.af28cf4e.chunk.js"
+  },
+  {
+    "revision": "4160dfb46474fa92543b",
+    "url": "/static/js/4.bb80ceb8.chunk.js"
+  },
+  {
+    "revision": "db8a30e653c541482a86",
+    "url": "/static/js/5.ab89304a.chunk.js"
+  },
+  {
+    "revision": "7d90f731adb96c7b86e3",
+    "url": "/static/js/6.91934930.chunk.js"
+  },
+  {
+    "revision": "b1c48719f63f74aca66d",
+    "url": "/static/js/7.f29dd876.chunk.js"
+  },
+  {
+    "revision": "0c786683b7c6294ce15d",
+    "url": "/static/js/main.be87a3e6.chunk.js"
+  },
+  {
+    "revision": "428ff75729f0a2bf3747",
+    "url": "/static/js/runtime~main.affb0451.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
